ae1f3bdd70852cce29d384e61f68aec5e8fa3d81
David Morton <davidmortonmail@gmail.com>
2015-04-28 21:07:17 +0100

    adding comments

--- src/Board.java
+++ src/Board.java
@@ -1,31 +1,338 @@
+import java.util.Arrays;
+
 public class Board{
-    public Board(int [][] tiles){
+	
+	private int N; // N x N tiles
+	private int[][] boardTiles; // tiles on the board
+	
+	/*
+	 * Create a board containing N x N tiles and 
+	 * populate it with the values stored in inputTiles.
+	 * Create a copy of the parameter board.
+	 * 
+	 * @param int [][] inputTiles is the input board from file
+	 * 
+	 */
+    public Board(int [][] inputTiles)
+    {
         // YOUR CODE HERE
+    	N = inputTiles.length; // N x N tiles
+    	boardTiles = new int[N][N]; // 2d array of tiles
+        
+        // create the N x N board of tiles
+        for (int i = 0; i < N; i++) 
+        {
+            for (int j = 0; j < N; j++) 
+            {
+            	boardTiles[i][j] = inputTiles[i][j];
+                //System.out.printf("%d", boardTiles[i][j]);
+            }
+            //System.out.printf("\n");
+        }           
     }
     
-    public int hamming(){
-        // YOUR CODE HERE
-        return 0;
+    /*
+     * How many tiles are not in the correct position
+     * in this board state. Loop through the board and calculate
+     * the number of tiles that are not in their goal states.
+     * 
+     * @return int hamming heuristic for this board
+     */
+    public int hamming()
+    {    	
+		int wrongPosCounter = 0;
+
+		for (int i = 0; i < N; i++) 
+		{
+			for (int j = 0; j < N; j++) 
+			{
+				// check if the tile is in the correct position
+				if (boardTiles[i][j] != correctTileAt(i, j)) 
+				{
+					// increment the counter
+					wrongPosCounter++;					
+				}
+			}
+		}
+		// subtract 1 for the blank square
+		return wrongPosCounter - 1;
     }
     
-    public int manhattan(){
-        // YOUR CODE HERE
-        return 0;
+    /*
+     * In the 2d array, the position (i, j) should contain
+     * the value i * N + j + 1
+     * e.g 	
+     * 		for position (0, 0) is 1
+     * 		0 * 3 + 0 + 1 = 1
+     * 
+     * 		for position (2, 1) = 8
+     * 		2 * 3 + 1 + 1 = 8
+     * 
+     * @param int i row position of tile in array
+     * @param int j column position of tile in array
+     * 
+     * @return int the goal position of the tile at the position (i, j)
+     */
+    private int correctTileAt(int i, int j) 
+    {    	
+        return i * N + j + 1;
     }
     
-    public boolean equals( Object o){
+    /*
+     * Calculate the Manhattan distance heuristic for this board.
+     * Sum the distance for each tile to its goal tile and use this
+     * to choose from the priority queue.
+     * 
+     * @return int sum of the Manhattan distances for this board
+     */
+    public int manhattan()
+    {    	  	
+		int sum = 0;
+		
+		// where the stored value should be in the board
+		int goalRow, goalCol;
+		
+		// For each tile in this board
+		for (int i = 0; i < N; i++)
+		{
+		    for (int j = 0; j < N; j++) 
+		    {		    			    	
+		        int tileNumber = boardTiles[i][j];
+		        if (tileNumber == 0) 
+		        {
+		        	// skip the blank square
+		        	continue;
+		        }
+		
+		        /*
+		         * Use the value stored in the array to caluclate
+		         * where it should be in the board
+		         * 
+		         * e.g 
+		         * 		value = 8
+		         * 		N = 3
+		         * 		
+		         * 		row = 8 - 1 / 3
+		         * 		row = 7 / 3
+		         * 		row = 2 (integer division)
+		         * 
+		         * 		column = 8 - 1 % 3
+		         * 		column = 7 % 3
+		         * 		column = 1 
+		         * 
+		         * 		tile 8 should be in position (2, 1)
+		         */
+		        goalRow = (tileNumber - 1) / N;
+		        goalCol = (tileNumber - 1) % N;
+		        
+		        /*
+		         *  Calculate the Manhattan distance from the tile at 
+		         *  (i, j) to its goal position (goalRow, goalCol). Use the 
+		         *  absolute values to get distance.
+		         *  
+		         *  e.g tile number 8
+		         *  	tile position (0, 0)
+		         *  	goal position for tile (2, 1)
+		         *  
+		         *  	manhattan = (0 - 2) + (0 - 1)
+		         *  	manhattan = -3
+		         *  	manhattan = absoluteValue(-3) = 3
+		         *  
+		         *  	if negative change to positive, if positive, do nothing.		
+		         */		        
+		        sum += Math.abs(i - goalRow) + Math.abs(j - goalCol);		        
+		    }
+	    }
+		return sum;        
+    }
+    
+    /**
+     * @return is this board the goal board?
+     */
+    public boolean isGoal() {
+    	StdOut.println("Checking if board is in the goal state...");
+    	    	
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                
+            	/*
+            	 * Check that all of the tiles in the board are in their correct positions, 
+            	 * except the empty tile that has a value of 0. Not checking the bottom right
+            	 * tile avoids the calculation error to find the empty tiles correct position
+            	 * 
+            	 * e.g	N = 3 
+            	 * 		find goal position of (2, 2) this should be the empty square 
+            	 * 		with value 0 in the bottom right of the goal state
+            	 * 
+            	 *  	equation : i * N + j + 1
+            	 *  
+            	 *  	2 * 3 + 2 + 1 = 9
+            	 *  
+            	 *  	The value at position 2,2 should be 0 not 9 in 
+            	 *  	the final goal state
+            	 */
+                if ((boardTiles[i][j] != correctTileAt(i, j)) && ((i != N - 1) || (j != N - 1))) {
+                    
+                	/*
+                	 * There is at least 1 tile wrong so the board is not in
+                	 * the goal state 
+                	 */
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    public boolean equals( Object o)
+    {
         // YOUR CODE HERE
-        return false;
+    	if (o == this) 
+    	{
+    		return true;
+    	}
+
+    	if (o == null) {
+    		return false;
+    	}
+
+    	if (o.getClass() != this.getClass()) {
+    		return false;
+    	}
+
+    	Board that = (Board) o;
+    	if (this.N != that.N) 
+    	{
+    		return false;
+    	}
+
+    	for (int i = 0; i < N; i++) 
+    	{
+    		if (!Arrays.equals(this.boardTiles[i], that.boardTiles[i])) 
+    		{
+    			return false;
+    		}
+    	}
+    	return true;        
     }
     
     public Iterable<Board> neighbours(){
         // YOUR CODE HERE
+    	for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+            	
+            	/*
+            	 * Walk through each tile in the board and if we hit
+            	 * the 0 tile we can move tiles into this space.
+            	 */
+                if (boardTiles[i][j] == 0) {
+                	StdOut.println("Creating queue of boards that have moved one tile into the empty tile space...");
+                	
+                	Queue<Board> q = new Queue<Board>();
+
+                	/*
+                	 * 
+                	 */
+                    if (i > 0) {
+                    	StdOut.println("Operator Down");
+                        q.enqueue(new Board(swap(i, j, i - 1, j)));
+                    }
+
+                    if (i < N - 1) {
+                    	StdOut.println("Operator Up");
+                        q.enqueue(new Board(swap(i, j, i + 1, j)));
+                    }
+
+                    if (j > 0) {
+                    	StdOut.println("Operator Right");
+                        q.enqueue(new Board(swap(i, j, i, j - 1)));
+                    }
+
+                    if (j < N - 1) {
+                    	StdOut.println("Operator Left");
+                        q.enqueue(new Board(swap(i, j, i, j + 1)));
+                    }
+                    StdOut.println("Returning queue of neightbour boards");
+                    return q;                    
+                }
+            }
+        }
+    	StdOut.println("Neighbours has returned null");
         return null;
     }
     
+    /*
+     * Create a twin of this board by swapping the first two adjacent tiles
+     * possible and returning that board. 
+     * 
+     * e.g 
+     * 		
+     */
+    public Board twin() {
+    	StdOut.println("Creating twin of board\n" + this);
+    	int N = boardTiles.length;
+        int[][] copy = new int[N][];
+        for (int i = 0; i < N; i++) {
+            copy[i] = Arrays.copyOf(boardTiles[i], N);
+        }
+                
+        // if N is either 0 or 1
+        if (N <= 1) {
+            return new Board(copy);
+        }
+
+        for (int i = 0; i < N; i++) {
+            int lastVal = boardTiles[i][0];
+
+            for (int j = 1; j < N; j++) {
+                int currentVal = boardTiles[i][j];
+
+                // if neither lastVal nor val is the blank square
+                if (currentVal != 0 && lastVal != 0) {
+                    copy[i][j] = lastVal;
+                    copy[i][j - 1] = currentVal;
+                    
+                    Board twinBoard = new Board(copy);
+                    StdOut.println("TWIN\n" + twinBoard);
+                    return twinBoard;
+                }
+
+                lastVal = currentVal;
+            }
+        }
+        
+    	return null;
+    }    
+    
+    /*
+     * 
+     */
+    private int[][] swap(int fromRow, int fromCol, int toRow, int toCol) {
+    	
+    	int N = boardTiles.length;
+        int[][] copy = new int[N][];
+        for (int i = 0; i < N; i++) {
+            copy[i] = Arrays.copyOf(boardTiles[i], N);
+        }
+        
+        
+        int tmp = copy[toRow][toCol];
+        copy[toRow][toCol] = copy[fromRow][fromCol];
+        copy[fromRow][fromCol] = tmp;
+        return copy;
+    }
+    
     public String toString(){
-        // YOUR CODE HERE
-        return "";
+        // YOUR CODE HERE    	
+    	StringBuilder s = new StringBuilder();
+        s.append(N + "\n");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                s.append(String.format("%2d ", boardTiles[i][j]));
+            }
+            s.append("\n");
+        }
+        return s.toString();       
     }
 }
         
--- src/Solver.java
+++ src/Solver.java
@@ -1,37 +1,278 @@
+/*
+ * Solver.java
+ * 
+ * The Solver class reads input from file and constructs a tile 
+ * puzzle with a single empty space denoted by the value 0. It then proceeds to 
+ * solve the puzzle using the A* algorithm. For each board that is created from
+ * an operator, up, down, left, right, a twin is created and checked. If a solution to a twin is found 
+ * then the initial puzzle is unsolvable. http://coursera.cs.princeton.edu/algs4/assignments/8puzzle.html.
+ * The Solver class uses the Board class that consists of a 2d array representing the tiles in
+ * the puzzle. 
+ * 
+ * Link to test cases : http://coursera.cs.princeton.edu/algs4/testing/8puzzle/ 
+ * 
+ * @author David Morton K00179391
+ * 
+ * Artificial Intelligence Programming Assignment 
+ * 8 Puzzle Problem
+ * 
+ */
 public class Solver{
-    public Solver(Board initial){
-        StdOut.println("Solver constructor called");    	
+	
+	// The goalStateNode is the Goal State search node when a solution is found
+	private Node goalStateNode;	
+	
+	// Flag to indicate an unsolvable puzzle
+	private boolean isSolvable; 
+	
+	/*
+	 * The Node class contains information about the board, number of moves
+	 * taken to get to this node, its parent node, and its heuristic value.
+	 * The node becomes part of a tree structure that is used by the A* algorithm 
+	 * to record the path from the initial state to the goal state. It implements
+	 * the Comparable interface that requires the implementation of the compareTo() function.
+	 */
+	private static class Node implements Comparable<Node> 
+	{        
+        private final Board board; 
+        private final int moves;       
+        private final Node previousNode;        
+        private final int priority;
+
+        /*
+         * Constructor for a Node that stores a board state, its parent
+         * Node, the number of moves taken to get to this node, and the 
+         * heuristic value of this Node
+         * 
+         * @param Board board for this node
+         * @param Node previousNode is the parent search node
+         */
+        private Node(Board board, Node previousNode)
+        {
+        	// Board state
+            this.board = board;
+            
+            // Parent search node
+            this.previousNode = previousNode;
+            
+            // The number of moves taken to get to this Node
+            this.moves = (this.previousNode == null) ? 0 : this.previousNode.moves + 1;
+            
+            // The heuristic used to order the search nodes in the priority queue
+            this.priority = this.board.manhattan() + this.moves;
+        }
+
+        /*
+         * compareTo function from Comparable interface(non-Javadoc)
+         * @see java.lang.Comparable#compareTo(java.lang.Object)
+         */
+        @Override
+        public int compareTo(Node that) 
+        {
+        	//StdOut.println("Re-arranging boards based on priority:\n" + this.board + "with\n" + that.board);
+        	StdOut.println("Re-arranging boards based on priority...");
+            return this.priority - that.priority;
+        }
+
+    }
+	
+	/*
+	 * Constructor for the Solver. 
+	 * 
+	 * @param Board initial the starting state of the board
+	 */
+    public Solver(Board initial)
+    {
+        StdOut.println("Solver constructor called"); 
+        
+        // Check to see if the initial board has already been solved
+        if(initial.isGoal())
+        {
+        	/*
+        	 * If the initial board is solved it is in 
+        	 * the goal state so there are no previousNode boards 
+        	 * in the solution. 
+        	 * 
+        	 * Set the final goalStateNode to a new search node 
+        	 * and set it's previousNode search node to null.        	 
+        	 */
+        	StdOut.println("Initial board is in goal state");
+        	goalStateNode = new Node(initial, null);
+        }
+        else
+        {
+        	/*
+        	 * If the initial board is not in its goal state
+        	 * start solving the board
+        	 */
+        	StdOut.println("Initial board is not in goal state, Solving...");
+        	goalStateNode = solve(initial, initial.twin());
+        }
     }
     
-    public boolean isSolvable(){
-    	StdOut.println("isSolvable called");
-        return false;
+    private Node solve(Board initial, Board twin) 
+    {    	
+    	StdOut.println("Creating Minimum Priority Queues for Initial and Twin boards");
+    	
+    	/*
+    	 * mainPQ queue used to store the nodes in the tree that have not yet been 
+    	 * expanded. The nodes with the smallest hueristic value will be checked 
+    	 * first.
+    	 */
+        MinPQ<Node> mainPQ = new MinPQ<Node>();
+        
+        /*
+         * twinPQ used in the same way as the mainPQ but starts with a twin 
+         * of the initial node. A twin is the initial with two adjecent tiles 
+         * swapped. 
+         */
+        MinPQ<Node> twinPQ = new MinPQ<Node>();
+        
+        // Add root Nodes to the priority queues
+        mainPQ.insert(new Node(initial, null));
+        twinPQ.insert(new Node(twin, null));
+               
+        StdOut.println("Root board in mainPQ is\n" + initial);
+        StdOut.println("Root board in TwinPQ is\n" + twin);
+        StdOut.println("Start loop until we find the goal board....");
+               
+        /*
+         * Loop until the initial board has been solved or the twin board
+         * takes the glory, meaning the puzzle cannot be solved.                  
+         */
+        while (true) 
+        {      
+        	StdOut.println("-----Start of mainQP check");
+        	// Check the board that has the smallest heuristic value in the main queue
+            Node node = nodeWithSmallestHeuristic(mainPQ);
+            
+            /*
+             *	Check that the node dequeued from the priority queue is the 
+             *	goal node. If it is we have solved the puzzle!. 
+             */
+            if (node.board.isGoal())
+            {            	
+            	StdOut.println("Found the board in the mainPQ!!");
+            	isSolvable = true;
+                return node;
+            }   
+                    
+            StdOut.println("-----End of mainQP check");                
+           
+            /*
+             * 	As described here 
+             * 	http://coursera.cs.princeton.edu/algs4/assignments/8puzzle.html
+             * 
+             *  If the twin of the initial puzzle is solvable it means that the 
+             *  initial puzzle is unsolvable, due to the fact that it is impossible 
+             *  to swap tiles directly without using the empty tile to do
+             *  so. If the twin of each board with one tile  swapped 
+             *  is solvable it means that the other board cannot be solved.   
+             */
+            StdOut.println("-----Start of twinPQ check");
+            if(nodeWithSmallestHeuristic(twinPQ).board.isGoal()) {
+            	StdOut.println("The puzzle is not solvable because we solved the twin :( ");
+                isSolvable = false;
+                return null;
+            }            
+            StdOut.println("-----End of twinPQ check!!");
+        }        
     }
     
-    public int moves(){
-    	StdOut.println("Solver moves called");
-        return 0;
+    private Node nodeWithSmallestHeuristic(MinPQ<Node> priorityQueue)
+    {
+    	StdOut.println("Finding the board in the priorityQueue with minimum priority...");
+    	Node least = priorityQueue.delMin();
+    	StdOut.println("Least priority board in queue found and removed from priorityQueue is\n" + least.board + "priorityQueue size: " + priorityQueue.size());
+    	
+    	/*
+    	 * For each board that is a neighbour of the least priority board
+    	 * get its neighbours and move to the next node with least priority.
+    	 * Expand the node an pick the next node that has the least heuristic cost
+    	 * so that we are moving closer to the goal state.  
+    	 */
+        for (Board neighbour : least.board.neighbours()) 
+        {            
+        	//StdOut.println("Neighbour found...");
+        	/*
+        	 * If this is the root board or the neighbour is not the same as the parent board,
+        	 * add it to the main priority queue. This is to avoid looping back
+        	 * to a board state that was previousNodely checked.
+        	 */
+            if (least.previousNode == null || !neighbour.equals(least.previousNode.board)) 
+            {
+            	StdOut.println(">Adding NEW board to priorityQueue \n" + neighbour);
+            	priorityQueue.insert(new Node(neighbour, least));
+            }
+        }
+        StdOut.println("priorityQueue size: " + priorityQueue.size());
+        
+        return least;
     }
     
-    public Iterable<Board> solution(){
-    	StdOut.println("Solver iterable solution called");
-        return null;
+    public boolean isSolvable()
+    {
+    	//StdOut.println("isSolvable called");
+    	return isSolvable;
+    }
+    
+    public int moves()
+    {
+    	//StdOut.println("Solver moves called");
+        return goalStateNode.moves;
+    }
+    
+    public Iterable<Board> solution()
+    {    	
+    	//StdOut.println("Solver iterable solution called");
+    	
+    	/*
+    	 *  Create a stack of board states that lead to the goal state. 
+    	 *  The solution is all of the board states that the initial 
+    	 *  board goes through to get to the goal state
+    	 *  
+    	 *  initial -> move one tile -> move one tile -> .... -> goal state
+    	 */
+    	Stack<Board> solution = new Stack<Board>();
+    	
+    	/*
+    	 * Start at the goal board and push onto the stack 
+    	 * each of the previousNode nodes leading up to the 
+    	 * goal board. 
+    	 */
+        for (Node sn = goalStateNode; sn != null; sn = sn.previousNode) 
+        {
+            solution.push(sn.board);
+        }
+        
+        // Return the stack of boards so they can printed from initial to goal 
+        return solution;        
     }
     
     public static void main(String [] args){
-        int N = StdIn.readInt();
-        int [][] tiles = new int [N][N];
+    	System.out.printf("Solver start\n");
+    	int N = StdIn.readInt();
+        int [][] tiles = new int [N][N];      
         
+        // Create the 2d array from the values stored on file
         for (int i = 0; i < N; i++){
             for (int j = 0; j < N; j++){
-                tiles[i][j] = StdIn.readInt();
-            }
+                tiles[i][j] = StdIn.readInt();                
+            }            
         }
         
+        // Create the initial board
         Board initial = new Board(tiles);
         
+        // Solve the initial board
         Solver solver = new Solver(initial);
         
+        /*
+         *  Loop through the boards in the stack and print
+         *  each board configuration. This will show which tile was moved
+         *  at each solution state from the initial board to the 
+         *  goal board. 
+         */        
         for (Board board : solver.solution()){
             System.out.println(board);
         }
